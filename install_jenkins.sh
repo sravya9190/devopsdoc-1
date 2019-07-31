73c54a738d50cc8582e6d6b630d3ee28ca25a8c3
manee2k6 <manee2k6@gmail.com>
2019-07-31 09:17:34 +0530

    Update install_jenkins.sh

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -32,3 +32,22 @@ sudo apt-get install jenkins -y
 sudo systemctl start jenkins
 sudo systemctl status jenkins
 sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+
+#Install Docker Engine
+sudo apt-get update -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update -y
+sudo apt-get install docker-ce -y
+sudo docker --version
+
+#Install Docker-Compose
+curl -L https://github.com/docker/compose/releases/download/1.24.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+cd /usr/local/bin
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+
